326581285125dfd2bf363d622744ef700500e961
"sudhi@herle.net" <"sudhi@herle.net">
2016-10-05 10:58:14 -0700

    don't use pid for android pkg

--- android/pkg/debug_linux.go
+++ android/pkg/debug_linux.go
@@ -30,8 +30,8 @@ func getself() *Pkg {
         return nil
     }
 
-    pid := fmt.Sprintf("caller-pid-%v", os.Getpid())
-    p := &Pkg{Name: pid, Uid: uint32(os.Getuid())}
-    return p
+    uid := uint32(os.Getuid())
+    nm  := fmt.Sprintf("caller-uid-%v", uid)
+    return &Pkg{Name: nm, Uid: uid}
 }
 
